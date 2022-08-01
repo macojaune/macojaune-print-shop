d9170dcbb0c290f0ea7456027e99dfe9a907210c
MarvinL.com <contact@marvinl.com>
2022-08-01 19:43:31 -0400

    update text

--- pages/merci/[amount].vue
+++ pages/merci/[amount].vue
@@ -3,7 +3,7 @@
   h1.text-8xl.text-white.text-center.text-amber-500.uppercase Merci
   p.text-white.text-center.mb-8 Il y a exactement 
     span.text-orange-400.text-3xl.font-bold {{amount}}
-    |  mercis sur cette page.
+    |  merci{{amount>0 ? 's': ''}} sur cette page.
   .flex.flex-wrap.gap-2.items-evenly
     span.p-4.text-white.animate-bounce(v-for="i in amount" class="cursor-default hover:cursor-default hover:text-amber-500 transition-colors delay-500") merci
   p.text-center.font-bold.text-orange-400 T'es vraiment en train de les compter là ? 😐😐
