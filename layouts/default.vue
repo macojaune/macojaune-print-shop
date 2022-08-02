c10e95b22f6ffa5a463fbc657f7b6cb923d3d014
MarvinL.com <contact@marvinl.com>
2022-08-01 21:23:01 -0400

    Add disclaimer msg Add Axeptio cookies

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,22 @@
 .main.flex.flex-col.justify-between.h-full.overflow-auto
   Header
   .container.mx-auto.flex-grow
-    //.p-3.rounded-md.bg-gradient-to-r.from-red-500.to-orange-500.text-black.font-semibold.text-center
+    .p-3.rounded-md.bg-gradient-to-r.from-red-500.to-orange-500.text-black.font-semibold.text-center
       | La boutique est momentanément suspendue. Les tirages reviennent bientôt
       |
       b.font-bold.text-white #stayConnect
     slot
   Footer
+  Script. 
+    window.axeptioSettings = {
+        clientId: "62e86de9c231db01ef04b2cc",
+      };
+
+    (function(d, s) {
+        let t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+        e.async = true; e.src = "//static.axept.io/sdk.js";
+        t.parentNode.insertBefore(e, t);
+      })(document, "script");
 </template>
 
 <script>
