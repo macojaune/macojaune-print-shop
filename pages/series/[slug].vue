02d3b557ec3fc944cf174df6e241c7cbbe081ad0
MarvinL.com <contact@marvinl.com>
2022-11-07 13:43:39 -0400

    fix imgs

--- pages/series/[slug].vue
+++ pages/series/[slug].vue
@@ -8,7 +8,7 @@
       ContentRenderer.text-white.text-lg(class="text-base md:text-lg" :value="doc")
       .picture-list.my-6.grid.grid-flow-row-dense.gap-4.justify-evenly.items-end(:class="`grid-cols-1 ${doc.products.length>2?'md:grid-cols-3':'md:grid-cols-2'}`")
         .picture(v-for="(product, index) in doc.products" :key="index" class="hover:cursor-pointer" )
-          nuxt-img.primary.border-radius.mb-2(v-if="product.images" :aspect-ratio="5/4" :src="`@/public/pictures${product.images[0]}`" fit="cover" @click="openModal(product)" itemprop="image" :alt="product.title")
+          nuxt-img.primary.border-radius.mb-2(v-if="product.images" :aspect-ratio="5/4" :src="`/pictures${product.images[0]}`" fit="cover" @click="openModal(product)" itemprop="image" :alt="product.title")
           p.text-2xl.text-center.text-amber-400.font-semibold(v-if="product.price") {{product.price}}€
             Meta(itemprop="price" :content="product.price")
             Meta(itemprop="priceCurrency" content="EUR")
@@ -51,33 +51,33 @@ definePageMeta({
 });
 const { title } = await queryContent("/runs/" + router.params.slug).findOne();
 useHead({
-  title: title + " - Macojaune.com",
+  title: title+' - Macojaune.com',
   script: [
     {
       type: "application/ld+json",
-      children: JSON.stringify({
-        "@context": "http://schema.org/",
-        "@type": "BreadcrumbList",
-
-        itemListElement: [
-          {
-            "@type": "ListItem",
-            position: 1,
-            item: {
-              "@id": "https://macojaune.com",
-              name: "Homepage",
+      children: JSON.stringify(
+        { "@context": "http://schema.org/",
+         "@type": "BreadcrumbList" ,
+        
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              item: {
+                "@id": "https://macojaune.com",
+                name: "Homepage",
+              },
             },
-          },
-          {
-            "@type": "ListItem",
-            position: 2,
-            item: {
-              "@id": "https://macojaune.com" + router.fullPath,
-              name: title,
+            {
+              "@type": "ListItem",
+              position: 2,
+              item: {
+                "@id": "https://macojaune.com" + router.fullPath,
+                name: title,
+              },
             },
-          },
-        ],
-      }),
+          ],
+        })
     },
   ],
 });
