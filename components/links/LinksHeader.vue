d29f8ef88ba5194443eff46d3125358443b74c94
macojaune <5950142+macojaune@users.noreply.github.com>
2022-11-08 15:39:46 -0400

    Update snipcart and open e-shop back (#41)
    * Add Snipcart
    Update header on Link page
    Update UI
    
    * move images
    
    * update UI
    add microdatas & seo meta
    
    * bump node vErsion
    
    * fix img
    add qty on latest serie
    
    * fix imgs
    
    * update name
    
    * update products
    
    * remove nuxt-img
    
    * Add product detail page
    Update serie 4
    update links
    
    * cleanup
    
    * cleanup

---
+++ components/links/LinksHeader.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+header(class="py-6 md:py-32")
+  NuxtLink.text-center.text-amber-400(class="hover:text-orange-600 transition-colors ease-in-out duration-150" to="/")
+    h1(class="text-5xl md:text-7xl") MACOJAUNE
+      small.text-2xl.text-grey-500 .COM
+    p.text-center T'as cliqué sur le lien dans ma bio ? Tu fais partie de l'élite.
+</template>
+
+<script setup></script>
+
+<style lang="stylus" scoped></style>
