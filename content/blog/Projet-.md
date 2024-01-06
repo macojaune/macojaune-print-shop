36f47c81a1892ff5b377f11bc977373d3ae09825
MarvinL.com <contact@marvinl.com>
2024-01-06 02:14:48 -0400

    wip: project

---
+++ content/blog/Projet-.md
@@ -0,0 +1,11 @@
+---
+title: '[Projet] '
+permalink: projet-
+date: 2024-01-06T05:21:34.634Z
+updatedAt: 2024-01-06T05:22:35.539Z
+layout: post
+categories:
+  - projet
+---
+
+test
--- pages/blog/[permalink].vue
+++ pages/blog/[permalink].vue
@@ -3,16 +3,34 @@
     <h2 class="font-display uppercase text-amber-600 lg:my-3 lg:text-7xl">
       {{ data?.title ?? "" }}
     </h2>
-    <div class="  lg:w-7/12 ">
+    <div class="lg:w-7/12">
       <ContentRenderer class="text-lg text-white" :value="data" />
+    </div>
+    <div v-if="isProject">
+      <h3 class="font-display my-2 text-2xl text-amber-400 lg:my-3 lg:text-3xl">
+        Je veux participer, comment on fait
+        ?
+      </h3>
+      <p class="text-xl text-white">
+        Très simple, contacte-moi via Instagram ou Telegram <span
+          class="text-sm italic text-orange-500"
+        >(ou
+          klaxonne
+          moi dans la
+          rue)</span>
+      </p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Article } from '~/types'
+
 const { params } = useRoute()
 const { data } = await useAsyncData('get-document', () =>
-  queryContent('/blog').where({ permalink: `${params?.permalink}` }).findOne())
+  queryContent<Article>('/blog').where({ permalink: `${params?.permalink}` }).findOne())
+
+const isProject = computed(() => !!data?.title?.startsWith('[PROJET]'))
 </script>
 
 <style scoped>
